c3a630e6fdf636d73da09a56b9fcd98e33221523
Helios <sandip.dey.tool@gmail.com>
2024-07-24 01:53:40 +0530

    commit1

--- example.py
+++ example.py
@@ -3,10 +3,12 @@ import image_dehazer
 
 if __name__ == "__main__":
 
-    HazeImg = cv2.imread('Images/foggy_oaks.jpg')						# read input image -- (**must be a color image**)
+    HazeImg = cv2.imread('Images/y01_photo.png')						# read input image -- (**must be a color image**)
     HazeCorrectedImg, haze_map = image_dehazer.remove_haze(HazeImg, showHazeTransmissionMap=False)		# Remove Haze
 
+   # cv2.imshow(HazeImg)
     cv2.imshow('haze_map', haze_map);						# display the original hazy image
+    cv2.imshow('Original Hazy Image', HazeImg)              # Display the original hazy image
     cv2.imshow('enhanced_image', HazeCorrectedImg);			# display the result
     cv2.waitKey(0)
     cv2.imwrite("outputImages/result.png", HazeCorrectedImg)
